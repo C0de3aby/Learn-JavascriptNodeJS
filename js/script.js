8ba4cbb8cfe729d7f2fa5c96572b51a580fcbcdc
c0de.3aby <c0de.3aby@gmail.com>
2023-02-06 21:12:51 +0700

    Lecture : Workshop Interaction

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 // javascript : Interaction
 
-let strFirstName = prompt('กรุณาป้อน : ชื่อ')
-let strLastName = prompt('กรุณาป้อน : นามสกุล')
+// let strFirstName = prompt('กรุณาป้อน : ชื่อ')
+// let strLastName = prompt('กรุณาป้อน : นามสกุล')
 
-alert(`สวัสดี : ${strFirstName} ${strLastName}`)
+// alert(`สวัสดี : ${strFirstName} ${strLastName}`)
+
+let x = prompt('x ?')
+let y = prompt('y ?')
+
+alert(`x : type ${typeof x}`)5
+alert(`y : type ${typeof y}`)
+
+alert(`${x} + ${y} = ${Number(x) + Number(y)}`)
+alert(`${x} - ${y} = ${Number(x) - Number(y)}`)
+alert(`${x} * ${y} = ${Number(x) * Number(y)}`)
+alert(`${x} / ${y} = ${Number(x) / Number(y)}`)
