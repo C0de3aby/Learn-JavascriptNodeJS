2d2c1c22bab37b75beb45d9a3a05e98ab48e00d4
c0de.3aby <c0de.3aby@gmail.com>
2023-02-06 16:13:28 +0700

    Lecture : Variable in javascript

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// let : ตัวแปรที่เปลี่ยนแปลงค่าได้
+
+let x = 11
+let y = 5
+let z = x + y
+
+console.log(z)
+
+x = 2
+y = 2
+z = x + y
+
+console.log(z)
+
+// const : ตัวแปรที่เปลี่ยนแปลงค่าได้
+
+const data = 2000
+
+console.log(data)
+
+// Workshop
+
+let salary = 18000
+let yearlyIncome = salary*12
+
+console.log(yearlyIncome)
