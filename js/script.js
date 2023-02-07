04fc1bb14774cb6e85b8ebcc55af9ff23f6fc78f
c0de.3aby <c0de.3aby@gmail.com>
2023-02-07 23:20:29 +0700

    Lecture : logic - if else - shorthand - 1

--- js/script.js
+++ js/script.js
@@ -1,47 +1,5 @@
-//---------------------------------------------------- 1
-// let a = 10
-// let b = 10
+let isAdmin = confirm('Are you admin ?')
 
-// console.log('a = b',a == b)
-// console.log('a = b',a != b)
-// console.log('a = b',a > b)
-// console.log('a = b',a >= b)
-// console.log('a = b',a < b)
-// console.log('a = b',a <= b)
+let result = isAdmin ? 'Administrator' : 'Member'
 
-// let age = 51
-
-// if(age >= 51){
-//     console.log('ผู้สูงอายุ')
-// }else if(age > 18){
-//     console.log('วัยรุ่น')
-// }else{
-//     console.log('เด็ก')
-// }
-
-//---------------------------------------------------- 2
-// let username = prompt('username')
-// let password = prompt('password')
-
-// if (username == 'admin' && password == '1234') {
-//   alert('Login Complet')
-// }else if (username != 'admin' ) {
-//   alert('Not username. Register Pls.')
-// }else{
-//   alert('Incorrect')
-// }
-
-//---------------------------------------------------- 3
-
-let javascript = confirm('JS ?')
-let python = confirm('PY ?')
-
-if(javascript || python){
-  alert('ok')
-}else{
-  alert('bye')
-}
-
-let stateBoolen = confirm('Select ?')
-
-alert(!stateBoolen)
+alert(result)
