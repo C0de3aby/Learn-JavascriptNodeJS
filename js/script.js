028d00e195191c1a29918a58e13f8e3dc77f74d5
c0de.3aby <c0de.3aby@gmail.com>
2023-02-07 23:26:31 +0700

    Lecture : logic - if else - shorthand workshop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-let isAdmin = confirm('Are you admin ?')
+// let isAdmin = confirm('Are you admin ?')
 
-let result = isAdmin ? 'Administrator' : 'Member'
+// let result = isAdmin ? 'Administrator' : 'Member'
+
+// alert(result)
+
+let question = `what is 5 + 10 = ?
+A. 7
+B. 8
+C. 9
+D. 16
+`
+
+let result = prompt(question) == 'C' ? 'Correct' : 'Incorrect'
 
 alert(result)
