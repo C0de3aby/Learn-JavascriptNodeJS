a19189ea8229f6c63a425773ac6fadddbaae7dff
c0de.3aby <c0de.3aby@gmail.com>
2023-02-08 14:06:58 +0700

    Lecture : for loop ( nested )

--- js/script.js
+++ js/script.js
@@ -1,46 +1,16 @@
-let arr = ['a','b','c','d','e']
-let obj = { a: 1,b: 2,c: 3,d: 4 }
-// let index = 0
+let game = [
+  ['o','x','x'],
+  ['o','o','x'],
+  ['x','x','o']
+]
 
-// while (index < arr.length) {
-//   console.log(arr[index])
-//   index++
-// }
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i])
-// }
-
-// for (let item of arr) {
-//   console.log(item)
-// }
-
-// for (let key in obj) {
-//   console.log(`${key} : ${obj[key]}`)
-// }
-
-// let dataSet = [1,2,3,4,5]
-// let result  = 0
-
-// for( let n of dataSet ){
-//   result += n
-// }
-
-// console.log(result)
-
-let dataSet = [1,2,3,4,5,-1,-3,10]
-
-let minResult = dataSet[0]
-let maxResult = dataSet[0]
-
-for (let n of dataSet){
-  if (n < minResult){
-    minResult = n
+let result = ''
+for ( let rowData of game){
+  let rowStr = ''
+  for ( let field of rowData){
+    rowStr += field + ' '
   }
-  if (n > maxResult){
-    maxResult = n
-  }
+  result += rowStr + '\n'
 }
 
-console.log('min : ',minResult)
-console.log('max : ',maxResult)
+console.log(result)
