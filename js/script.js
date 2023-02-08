303af148a0385abfc87c097dd4c45a07cd5e0c87
c0de.3aby <c0de.3aby@gmail.com>
2023-02-08 08:36:06 +0700

    Lecture : switch case

--- js/script.js
+++ js/script.js
@@ -4,13 +4,30 @@
 
 // alert(result)
 
-let question = `what is 5 + 10 = ?
-A. 7
-B. 8
-C. 9
-D. 16
-`
+// let question = `what is 5 + 10 = ?
+// A. 7
+// B. 8
+// C. 9
+// D. 16
+// `
 
-let result = prompt(question) == 'C' ? 'Correct' : 'Incorrect'
+// let result = prompt(question) == 'C' ? 'Correct' : 'Incorrect'
 
-alert(result)
+// alert(result)
+
+let input = 1
+
+switch(input){
+  case 1 :
+    console.log('one')
+    break
+  case 2 :
+    console.log('twe')
+    break
+  case 3 :
+    console.log('three')
+    break
+  default:
+    console.log('error')
+    break
+}
