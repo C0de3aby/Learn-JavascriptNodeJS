af59e59f7c1c58ffc9a27235d0a98743e4bce582
c0de.3aby <c0de.3aby@gmail.com>
2023-02-08 14:33:42 +0700

    Lecture : for loop ( nested ) 2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-let game = [
-  ['o','x','x'],
-  ['o','o','x'],
-  ['x','x','o']
-]
+// let game = [
+//   ['o','x','x'],
+//   ['o','o','x'],
+//   ['x','x','o']
+// ]
 
+// let result = ''
+// for ( let rowData of game){
+//   let rowStr = ''
+//   for ( let field of rowData){
+//     rowStr += field + ' '
+//   }
+//   result += rowStr + '\n'
+// }
+
+// console.log(result)
+
+let rowCount = 6
 let result = ''
-for ( let rowData of game){
-  let rowStr = ''
-  for ( let field of rowData){
-    rowStr += field + ' '
-  }
-  result += rowStr + '\n'
-}
 
+for ( let i = 0 ; i <= rowCount ; i++ ){
+  let data = ''
+  for (let n = 0 ; n < i ; n ++ ) {
+    data += '*'
+    //console.log(data)
+  }
+  result += data+'\n'
+}
 console.log(result)
