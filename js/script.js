9e73df2cd82ad4d8c96b8f0743de4448f8dede6f
c0de.3aby <c0de.3aby@gmail.com>
2023-02-08 09:48:45 +0700

    Lecture : strict comparison

--- js/script.js
+++ js/script.js
@@ -1,33 +1,6 @@
-// let isAdmin = confirm('Are you admin ?')
+let a = '00001234'
+let b = 1234
 
-// let result = isAdmin ? 'Administrator' : 'Member'
-
-// alert(result)
-
-// let question = `what is 5 + 10 = ?
-// A. 7
-// B. 8
-// C. 9
-// D. 16
-// `
-
-// let result = prompt(question) == 'C' ? 'Correct' : 'Incorrect'
-
-// alert(result)
-
-let input = 1
-
-switch(input){
-  case 1 :
-    console.log('one')
-    break
-  case 2 :
-    console.log('twe')
-    break
-  case 3 :
-    console.log('three')
-    break
-  default:
-    console.log('error')
-    break
-}
+// console.log(a = b) // b move data 1234 to a
+console.log(a == b) // true.
+console.log(a === b) // false . because === check data type
