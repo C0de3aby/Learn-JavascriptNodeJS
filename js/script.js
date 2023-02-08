836326d52070b42c61ed8d28f6474c77d0543c94
c0de.3aby <c0de.3aby@gmail.com>
2023-02-08 09:58:38 +0700

    Lecture : default value

--- js/script.js
+++ js/script.js
@@ -1,6 +1,2 @@
-let a = '00001234'
-let b = 1234
-
-// console.log(a = b) // b move data 1234 to a
-console.log(a == b) // true.
-console.log(a === b) // false . because === check data type
+let result = prompt('input int value :') || 10
+alert(result)
