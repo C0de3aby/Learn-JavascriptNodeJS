7ca90c65268bf55930359561c7eba6e5910dd173
c0de.3aby <c0de.3aby@gmail.com>
2023-02-12 18:31:47 +0700

    Lecture : DOM 2 - Workshop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-let strName = prompt('myName')
-let myName = document.getElementById('myName')
+let elForm = document.querySelector('form')
+let elSpanResult = document.getElementById('result')
 
-myName.innerText = strName
+elForm.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  let el = e.target
+  let elInputX = Number(el.querySelector('[name="x"]').value)
+  let elInputY = Number(el.querySelector('[name="y"]').value)
+  let elSelectOperator = el.querySelector('[name="operator"]').value
+  let output
+  let strOperator
+  // console.log(`${elInputX} ${elSelectOperator} ${elInputY}`)
+  switch (elSelectOperator) {
+    case 'plus':
+      output = elInputX + elInputY
+      strOperator = '+'
+      break;
+    case 'minus':
+      output = elInputX - elInputY
+      strOperator = '+'
+      break;
+    case 'multiply':
+      output = elInputX * elInputY
+      strOperator = '×'
+      break;
+    case 'divide':
+      output = elInputX / elInputY
+      strOperator = '÷'
+      break;
+  }
+  elSpanResult.innerText = `${elInputX} ${strOperator} ${elInputY} = ${output}`
+})
