03704b205851c6740838bce23c29f64d127c3c37
c0de.3aby <c0de.3aby@gmail.com>
2023-02-12 21:37:49 +0700

    Lecture : DOM 3 - Workshop

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
-let elForm = document.querySelector('form')
-let elSpanResult = document.getElementById('result')
+let a = document.links
 
-elForm.addEventListener('submit', (e) =>{
-  e.preventDefault()
-  let el = e.target
-  let elInputX = Number(el.querySelector('[name="x"]').value)
-  let elInputY = Number(el.querySelector('[name="y"]').value)
-  let elSelectOperator = el.querySelector('[name="operator"]').value
-  let output
-  let strOperator
-  // console.log(`${elInputX} ${elSelectOperator} ${elInputY}`)
-  switch (elSelectOperator) {
-    case 'plus':
-      output = elInputX + elInputY
-      strOperator = '+'
-      break;
-    case 'minus':
-      output = elInputX - elInputY
-      strOperator = '-'
-      break;
-    case 'multiply':
-      output = elInputX * elInputY
-      strOperator = '×'
-      break;
-    case 'divide':
-      output = elInputX / elInputY
-      strOperator = '÷'
-      break;
-  }
-  elSpanResult.innerText = `${elInputX} ${strOperator} ${elInputY} = ${output}`
-})
+for(let el of a) {
+  el.setAttribute('target', '_bank')
+}
